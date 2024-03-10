038b0b70451829a18f6ed1f7bcf3e3642bf500d7
AcciGen <denrov13@gmail.com>
2024-03-10 11:33:48 +0500

    ready to production

--- RecipeManagement.API/Program.cs
+++ RecipeManagement.API/Program.cs
@@ -81,7 +81,7 @@ namespace RecipeManagement.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
